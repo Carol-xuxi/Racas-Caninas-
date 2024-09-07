8084babfccf055746099472ff3c9423e3a67028a
Carol-xuxi <ca12rol.css@gmail.com>
2024-09-07 16:52:32 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,140 @@
+let dados = [ {
+  titulo: "Lulu da Pomerânia",
+  descricao: "O Lulu da Pomerânia, também conhecido como Spitz Alemão Anão, é uma raça de cão de companhia pequeno e ativo, com pelagem densa e fofa. Apesar do tamanho, são cães corajosos e inteligentes, ideais para apartamentos. São conhecidos por sua personalidade extrovertida e lealdade ao dono.",
+  link: "https://pt.wikipedia.org/wiki/Spitz_alemão",
+  tags: "raposinha"
+},
+  {
+    titulo: "Pug",
+    descricao: "O Pug é uma raça de cão braquicefálico, conhecido por sua aparência charmosa e personalidade brincalhona. Originário da China, é um cão de companhia popular por sua lealdade e adaptabilidade. Apesar de sua aparência delicada, o Pug é um cão robusto e cheio de energia.",
+    link: "https://pt.wikipedia.org/wiki/Pug",
+    tags: "cachorro filme Homens de Preto"
+  },
+  {
+    titulo: "Dachshund",
+    descricao: "O Dachshund, também conhecido como salsicha, é uma raça de cão de caça com pernas curtas e corpo alongado. Sua personalidade é curiosa e determinada, e são excelentes cães de companhia. Existem três tipos de pelagem: lisa, de pelo duro e longa.",
+    link: "https://pt.wikipedia.org/wiki/Dachshund",
+    tags: "salsichinha"
+  },
+      
+            {
+              titulo: "Pitbull",
+              descricao: "O Pitbull é uma raça forte e leal, muitas vezes incompreendida. É um excelente cão de família quando bem socializado.",
+              link: "https://pt.wikipedia.org/wiki/Pitbull",
+              tags: "cão musculoso"
+            },
+
+            {
+              titulo: "Maltês",
+              descricao: "O Maltês é um cão de companhia pequeno e elegante, conhecido por sua pelagem branca e fofa.",
+              link: "https://pt.wikipedia.org/wiki/Maltês",
+              tags: "cão com pêlo perolado"
+            },
+
+            {
+              titulo: "Boxer",
+              descricao: "O Boxer é um cão atlético e enérgico, conhecido por sua lealdade e inteligência.",
+              link: "https://pt.wikipedia.org/wiki/Boxer",
+              tags: "braquiocefálico cão babão"
+          
+            },
+            
+            {
+              titulo: "Sem Raça Definida ou Vira Lata",
+              descricao: "Cães SRD são tão especiais quanto os cães de raça. Cada um tem sua personalidade única.",
+              link: "https://pt.wikipedia.org/wiki/Cão_vira-lata",
+              tags: "melhor raça vira-lata caramelo"
+            },
+
+            {
+                titulo: "Cocker spaniel",
+                descricao: "O Cocker spaniel é um cão de caça de porte médio, conhecido por sua pelagem longa e sedosa.",
+                link: "https://pt.wikipedia.org/wiki/Cocker_spaniel",
+                tags: "fofinho"
+              },
+              
+              {
+                titulo: "Shi Tzu",
+                descricao: "O Shi Tzu é um cão de companhia pequeno e charmoso, conhecido por sua pelagem longa e sedosa.",
+                link: "https://pt.wikipedia.org/wiki/Shi_tzu",
+                tags: "braquiocefálico cão chinês"
+              },
+
+              {
+                titulo: "Yorkshire",
+                descricao: "O Yorkshire Terrier é um cão de companhia pequeno e ativo, com pelagem longa e sedosa, geralmente de cor azul e castanho.",
+                link: "https://pt.wikipedia.org/wiki/Yorkshire_terrier",
+                tags: "cão dengoso"
+              },
+
+              {
+                titulo: "Pastor Alemão",
+                descricao: "O Pastor Alemão é um cão de trabalho inteligente e versátil, conhecido por sua lealdade e coragem.",
+                link: "https://pt.wikipedia.org/wiki/Pastor_alemão",
+                tags: "cão de guarda cão policial"
+              },
+              {
+                titulo: "Rottweiler",
+                descricao: "O Rottweiler é um cão de guarda forte e poderoso, mas também pode ser um companheiro leal e protetor.",
+                link: "https://pt.wikipedia.org/wiki/Rottweiler",
+                tags: "cão de guarda"
+              },
+
+              {
+                titulo: "Bulldog Francês",
+                descricao: "O Bulldog Francês é um cão de companhia charmoso e brincalhão, conhecido por sua aparência achatada.",
+                link: "https://pt.wikipedia.org/wiki/Bulldog_francês",
+                tags: "braquiocefálico focinho achatado cão roncador"
+              },
+
+              {
+                titulo: "Bulldog Inglês",
+                descricao: "O Bulldog Inglês é um cão de companhia musculoso e de aparência distinta, conhecido por sua personalidade calma e gentil.",
+                link: "https://pt.wikipedia.org/wiki/Bulldog_inglês",
+                tags: "braquiocefálico focinho achatado cão roncador"
+              },
+
+              {
+                titulo: "Corgi",
+                descricao: "O Corgi é um cão de pastoreio de pernas curtas, conhecido por sua inteligência e personalidade alegre.",
+                link: "https://pt.wikipedia.org/wiki/Corgi",
+                tags: "cão da rainha Elizabeth"
+              },
+
+              {
+                titulo: "Poodle",
+                descricao: "O Poodle é uma raça inteligente e versátil, disponível em três tamanhos: padrão, miniatura e toy.",
+                link: "https://pt.wikipedia.org/wiki/Poodle",
+                tags: "cão pompom branco"
+              },
+
+              {
+                titulo: "Rhodesian Ridgeback",
+                descricao: "O Rhodesian Ridgeback é um cão de caça africano, conhecido por sua força, velocidade e pelagem única com uma crista ao longo do dorso.",
+                link: "https://pt.wikipedia.org/wiki/Rhodesian_ridgeback",
+                tags: "cão africano cão exposições"
+              },
+
+              {
+                titulo: "Labrador",
+                descricao: "O Labrador Retriever é uma raça popular e versátil, conhecida por sua natureza amigável e inteligência.",
+                link: "https://pt.wikipedia.org/wiki/Labrador_retriever",
+                tags: "cão dócil"
+              },
+
+              {
+                titulo: "Golden Retriever",
+                descricao: "O Golden Retriever é um cão de companhia e trabalho popular, conhecido por sua pelagem dourada e personalidade gentil.",
+                link: "https://pt.wikipedia.org/wiki/Golden_retriever",
+                tags: "cão dócil amantes de água"
+              },
+
+              {
+                titulo: "West Highland White Terrier",
+                descricao: "O West Highland White Terrier é um cão pequeno e ativo, conhecido por sua pelagem branca e personalidade forte.",
+                link: "https://pt.wikipedia.org/wiki/West_Highland_White_Terrier",
+                tags: "cão furacão"
+              },
+          
+      ];
+
